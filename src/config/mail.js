246ed3f45b85273f7af582fcb9cda7fdcf327a22
costaarthur <costaarthur@hotmail.com>
2020-01-27 17:17:48 -0300

    mailer fila fails schedule list schedule virtual

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4d843ed74ce5d2',
+    pass: '6f5aa1dd33ba5d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
