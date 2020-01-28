b7bb40d41f0ea14ebc0924180f6b8413a206b217
costaarthur <costaarthur@hotmail.com>
2020-01-28 16:25:43 -0300

    env error check gg b-end

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4d843ed74ce5d2',
-    pass: '6f5aa1dd33ba5d',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
